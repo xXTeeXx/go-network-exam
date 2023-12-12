4a031d33341c49dc5600728e8277823ed5fea593
xXTeeXx <woramett2015@gmail.com>
2023-12-12 13:24:07 +0700

    update test

--- exam-1/main_test.go
+++ exam-1/main_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestExampleFunction(t *testing.T) {
     // TODO: Implement tests for the student's code
     // Example test (adjust according to the actual assignment)
-    expected := "Hello, Go!"
+    expected := "Hello, "
     if got := HelloFunction(); got != expected {
         t.Errorf("HelloFunction() = %v, want %v", got, expected)
     }
